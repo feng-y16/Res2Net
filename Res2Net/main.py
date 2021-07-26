3387d871e46e6441d9c67e4f793c26ce94a0cdf4
Yao <y-feng20@mails.tsinghua.edu.cn>
2021-07-26 12:51:29 +0800

    add saving data and figures

--- Res2Net/main.py
+++ Res2Net/main.py
@@ -1,28 +1,36 @@
+import os
+import sys
 import numpy as np
 import argparse
+import random
 import torch
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 import pdb
 from nn_models.ode import FirstOrderODENet, SecondOrderODENet
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--task', type=str, default='Pendulum-v0')
+    parser.add_argument('--task', type=str, default='ODE')
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--input-dim', type=int, default=64)
     parser.add_argument('--hidden-dim', type=int, default=256)
     parser.add_argument('--data-size', type=int, default=2000)
     parser.add_argument('--input-noise', type=float, default=0)
     parser.add_argument('--lr', type=float, default=1e-3)
-    parser.add_argument('--total-batches', type=float, default=1000)
+    parser.add_argument('--train-split', type=float, default=0.8)
+    parser.add_argument('--total-batches', type=float, default=500)
     parser.add_argument('--batch-size', type=int, default=256)
-    parser.add_argument('--model', type=str, default='FirstOrderODENet')
+    parser.add_argument('--model-name', type=str, default='FirstOrderODENet')
+    parser.add_argument('--result-path', type=str, default='../results')
+    parser.add_argument('--data-path', type=str, default='../data')
     parser.add_argument(
         '--device', type=str,
         default='cuda' if torch.cuda.is_available() else 'cpu')
     args = parser.parse_known_args()[0]
     assert args.input_dim % 2 == 0
+    args.device = 'cpu'
     return args
 
 
@@ -39,32 +47,94 @@ def generate_data(input_dim, data_size):
     return np.concatenate((x, dx), axis=1), np.concatenate((y, dy), axis=1)
 
 
-def main(args):
-    x, y = generate_data(args.input_dim, args.data_size)
+def draw(stats, save_file_wo_ext):
+    plt.rcParams['font.sans-serif'] = ['Times New Roman']
+    plt.rcParams.update({'figure.autolayout': True})
+    plt.rc('font', size=23)
+    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(16, 9))
+    iterations = np.arange(1, len(stats['train_loss']) + 1)
+    axes.plot(iterations, stats['train_loss'][:, 0], label='train')
+    axes.plot(iterations, stats['test_loss'][:, 0], label='test')
+    axes.set_xlabel('Iterations')
+    axes.set_ylabel('Loss')
+    ax.grid(True)
+    ax.legend(loc='best')
+    plt.savefig(save_file_wo_ext + '.png')
+    plt.savefig(save_file_wo_ext + '.pdf')
+    plt.close()
+
+
+def train(args):
+    os.environ['PYTHONHASHSEED'] = '0'
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    np.random.seed(args.seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+    if not os.path.isdir(args.data_path):
+        os.makedirs(args.data_path)
+    data_file_path = os.path.join(args.data_path, 'data.npz')
+    if os.path.isfile(data_file_path):
+        x = np.load(data_file_path)['x']
+        y = np.load(data_file_path)['y']
+    else:
+        x, y = generate_data(args.input_dim, args.data_size)
+        np.savez(data_file_path, x=x, y=y)
     x, y = torch.tensor(x).to(args.device).float(), torch.tensor(y).to(args.device).float()
-    # model = FirstOrderODENet(args.input_dim, args.hidden_dim, args.lr).to(args.device)
-    model = SecondOrderODENet(args.input_dim, args.hidden_dim, args.lr).to(args.device)
+    x += args.input_noise * torch.randn(*x.shape).to(args.device)
+    train_index = np.arange(0, int(x.shape[0] * args.train_split))
+    test_index = np.arange(int(x.shape[0] * args.train_split), x.shape[0])
+    train_x = x[train_index]
+    train_y = y[train_index]
+    test_x = x[test_index]
+    test_y = y[test_index]
+
+    if args.model_name == 'FirstOrderODENet':
+        model = FirstOrderODENet(args.input_dim, args.hidden_dim, args.lr).to(args.device)
+    else:
+        model = SecondOrderODENet(args.input_dim, args.hidden_dim, args.lr).to(args.device)
+
     stats = {'train_loss': [], 'test_loss': []}
     with tqdm(total=args.total_batches) as t:
         for step in range(args.total_batches):
-            # train step
-            ixs = torch.randperm(x.shape[0])[:args.batch_size]
-            loss = model.forward_train(x[ixs] + args.input_noise * torch.randn(*x[ixs].shape).to(args.device),
-                                       y[ixs] + args.input_noise * torch.randn(*y[ixs].shape).to(args.device))
-            train_loss = model.forward_train(x, y, False, False).cpu().numpy()
-            # test_loss = model.forward_train(test_x, test_next_x, False, False).cpu().numpy()
+            ixs = torch.randperm(train_x.shape[0])[:args.batch_size]
+            model.forward_train(train_x[ixs], train_y[ixs])
+            train_loss = model.forward_train(train_x, train_y, False, False).cpu().numpy()
+            test_loss = model.forward_train(test_x, test_y, False, False).cpu().numpy()
             stats['train_loss'].append([train_loss.mean(), train_loss.std()])
-            # stats['test_loss'].append([test_loss.mean(), test_loss.std()])
-            t.set_postfix(train_loss='{:.9f}'.format(train_loss.mean()))
-            # test_loss='{:.9f}'.format(test_loss.mean()))
-            # if args.verbose and step % args.print_every == 0:
-            #     # run validation
-            #     test_ixs = torch.randperm(test_x.shape[0])[:args.batch_size]
-            #     test_loss = model.forward_train(test_x[test_ixs], test_next_x[test_ixs], False)
-            #     print("step {}, train_loss {:.4e}, test_loss {:.4e}"
-            #           .format(step, loss.item(), test_loss.item()))
+            stats['test_loss'].append([test_loss.mean(), test_loss.std()])
+            t.set_postfix(train_loss='{:.9f}'.format(train_loss.mean()), test_loss='{:.9f}'.format(test_loss.mean()))
             t.update()
+    stats['train_loss'] = np.array(stats['train_loss'])
+    stats['test_loss'] = np.array(stats['test_loss'])
+    return stats
+
+
+def main(args, redirect=True):
+    save_path = '../results/' + args.model_name
+    if not os.path.isdir(save_path):
+        os.makedirs(save_path)
+    if redirect:
+        stdout = sys.stdout
+        f = open(os.path.join(save_path, 's' + str(args.seed) + '.txt'), 'w+')
+        sys.stdout = f
+    else:
+        f = None
+        stdout = None
+    stats = train(args)
+    if redirect:
+        f.close()
+        sys.stdout = stdout
+
+    save_stats_file = os.path.join(save_path, 's' + str(args.seed) + '.pl')
+    save_drawing_file_wo_ext = os.path.join(save_path, 's' + str(args.seed))
+    draw(stats, save_drawing_file_wo_ext)
+    with open(save_stats_file, 'w') as f:
+        pickle.dump(stats, f, protocol=4)
 
 
 if __name__ == '__main__':
-    main(get_args())
+    main(get_args(), False)
--- Res2Net/nn_models/mlp.py
+++ Res2Net/nn_models/mlp.py
@@ -101,7 +101,7 @@ class SecondOrderAutoEncoder(torch.nn.Module):
         assert input_dim % 2 == 0
         self.ae = MLPAutoEncoder(input_dim, hidden_dim, input_dim // 2, non_linearity=non_linearity)
         self.alpha = torch.nn.Parameter(-torch.randn(1, input_dim // 2))
-        self.beta = 0
+        self.beta = torch.nn.Parameter(-torch.randn(1, input_dim // 2))
 
     def forward(self, t, x):
         size = x.size()
